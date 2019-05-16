468b59a886599d7fcb6766d6bb5466309db0bf7f
Marcel Alves <marcel.alves89@gmail.com>
2019-05-16 16:45:05 -0300

    Connect storyboard elements to view controller
    Create usernameTextField outlet and searchButtonTapped action in HomeScreenViewController, then connects the storyboard respective elements to them.

--- AgileContentProject/Modules/HomeScreen/View/HomeScreenViewController.swift
+++ AgileContentProject/Modules/HomeScreen/View/HomeScreenViewController.swift
@@ -1,11 +1,17 @@
 import UIKit
 
 class HomeScreenViewController: UIViewController {
+    
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        // TODO: Implement search button action
+    }
 }
 
