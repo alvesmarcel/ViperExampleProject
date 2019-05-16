223fa0f3ece750c614d0ff62208a930ca975f43a
Marcel Alves <marcel.alves89@gmail.com>
2019-05-16 16:53:50 -0300

    Create and implement HomeScreenViewInterface
    HomeScreenViewController implements HomeScreenViewInterface, allowing the presenter to send error messages to the view controller.

--- AgileContentProject/Modules/HomeScreen/View/HomeScreenViewController.swift
+++ AgileContentProject/Modules/HomeScreen/View/HomeScreenViewController.swift
@@ -15,3 +15,10 @@ class HomeScreenViewController: UIViewController {
     }
 }
 
+extension HomeScreenViewController: HomeScreenViewInterface {
+    func showErrorMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+}
---
+++ AgileContentProject/Modules/HomeScreen/View/HomeScreenViewInterface.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+protocol HomeScreenViewInterface {
+    func showErrorMessage(title: String, message: String)
+}
